e0a9abcee8a47ea7ad6b60e998a4d822d125ffb8
tksbng <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-10-10 13:54:03 +0900

    for github

--- Counter/ViewController.m
+++ Counter/ViewController.m
@@ -12,12 +12,89 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    int count;
+    
+    NSString *countLabelText;
+    
+    IBOutlet UILabel *countLabel;
+    
+    IBOutlet UIButton *plus;
+    IBOutlet UIButton *minus;
+    IBOutlet UIButton *reset;
+}
+
+-(IBAction)plusPushed:(id)sender {
+    count = count + 1;
+    countLabelText = [NSString stringWithFormat:@"%d",count];
+    [countLabel setText:countLabelText];
+    if (count >= 0 && count <10) {
+        countLabel.textColor = [UIColor whiteColor];
+    
+    }else if(count >= 10 && count < 20)
+    {
+        countLabel.textColor = [UIColor greenColor];
+    
+    }else if (count >=20 && count <30)
+    {
+        countLabel.textColor = [UIColor yellowColor];
+    } else {
+        countLabel.textColor = [UIColor redColor];
+    }
+    
+    if (count % 3 == 0) {
+        countLabelText = @"さん";
+        [countLabel setText:countLabelText];
+      //  countLabel.minimumScaleFactor = 0.5f;
+        countLabel.adjustsFontSizeToFitWidth=YES;
+    }
+     
+    
+        
+}
+
+-(IBAction)minusPushed:(id)sender{
+    if (count > 0) {
+        count = count - 1;
+    }
+    countLabelText = [NSString stringWithFormat:@"%d",count];
+    [countLabel setText:countLabelText];
+    if (count >= 0 && count <10) {
+        countLabel.textColor = [UIColor whiteColor];
+        
+    }else if(count >= 10 && count < 20)
+    {
+        countLabel.textColor = [UIColor greenColor];
+        
+    }else if (count >=20 && count <30)
+    {
+        countLabel.textColor = [UIColor yellowColor];
+    } else {
+        countLabel.textColor = [UIColor redColor];
+        
+    }
+    if (count % 3 == 0) {
+        countLabelText = @"さん";
+        [countLabel setText:countLabelText];
+        //  countLabel.minimumScaleFactor = 0.5f;
+        countLabel.adjustsFontSizeToFitWidth=YES;
+    }
+    
+}
+
+-(IBAction)resetPushed:(id)sender {
+    count = 0;
+    countLabelText = [NSString stringWithFormat:@"%d",count];
+    [countLabel setText:countLabelText];
+    countLabel.textColor = [UIColor whiteColor];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    count = 0;
+    
+
 }
 
 - (void)didReceiveMemoryWarning
